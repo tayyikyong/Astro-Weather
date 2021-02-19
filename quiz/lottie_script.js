f25a22083b7b53a9fc5d62f297309131c1ca6368
HongYih <73088531+HongYih@users.noreply.github.com>
2021-02-19 11:04:45 +0800

    Delete quiz directory

--- quiz/lottie_script.js
+++
@@ -1,5 +0,0 @@
-setTimeout(myFunction, 5000);
-
-function myFunction(){
-  window.location.href = "quiz_index.html";
-}
--- quiz/lottie_script2.js
+++
@@ -1,5 +0,0 @@
-setTimeout(myFunction, 5000);
-
-function myFunction(){
-  window.location.href = "../index.html";
-}
